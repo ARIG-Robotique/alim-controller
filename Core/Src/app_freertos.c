b2d345e87c08590c750f93c95b2e46ea4f03d0e5
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-04-05 02:03:03 +0200

    Sample CAN implementation

--- Core/Src/app_freertos.c
+++ Core/Src/app_freertos.c
@@ -121,18 +121,15 @@ void StartDefaultTask(void *argument)
   //HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
   __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 27450);
 
-
-
   HAL_FDCAN_Start(&hfdcan1);
 
   FDCAN_TxHeaderTypeDef   TxHeader;
   uint8_t               TxData[8];
 
   /* Prepare Tx Header */
-  TxHeader.Identifier = 0x321;
   TxHeader.IdType = FDCAN_STANDARD_ID;
   TxHeader.TxFrameType = FDCAN_DATA_FRAME;
-  TxHeader.DataLength = FDCAN_DLC_BYTES_2;
+  TxHeader.DataLength = FDCAN_DLC_BYTES_1;
   TxHeader.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
   TxHeader.BitRateSwitch = FDCAN_BRS_OFF;
   TxHeader.FDFormat = FDCAN_CLASSIC_CAN;
@@ -141,49 +138,51 @@ void StartDefaultTask(void *argument)
 
   uint8_t nbCanMessage = 0;
 
+  uint16_t canId = 100;
+
   for(;;) {
+    HAL_GPIO_TogglePin(HEART_BEAT_GPIO_Port, HEART_BEAT_Pin);
 
-    int x;
-    //for(x=10; x<30; x++)
-    //{
-      //__HAL_TIM_SET_AUTORELOAD(&htim2, x*2);
-      //__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, x);
-      //HAL_Delay(100);
-    //}
-
-    i++;
-    if (i % 2) {
-      HAL_GPIO_TogglePin(HEART_BEAT_GPIO_Port, HEART_BEAT_Pin);
-      //HAL_GPIO_TogglePin(AU_KO_GPIO_Port, AU_KO_Pin);
-      //HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-    } else {
-      //HAL_GPIO_TogglePin(AU_OK_GPIO_Port, AU_OK_Pin);
-      //HAL_GPIO_TogglePin(WARN_BATT_GPIO_Port, WARN_BATT_Pin);
-      //HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_1);
-    }
-    if (i % 4) {
-      HAL_GPIO_TogglePin(PIL_ALIM_2_GPIO_Port, PIL_ALIM_2_Pin);
-
-      TxData[0] = nbCanMessage;
-      nbCanMessage++;
-      if (nbCanMessage > 10) {
-        nbCanMessage = 0;
+    TxHeader.Identifier = canId;
+    TxData[0] = nbCanMessage;
+    //TxData[1] = nbCanMessage * 2;
+    nbCanMessage++;
+    if (nbCanMessage > 16) {
+      nbCanMessage = 0;
+      canId++;
+      if (canId == 102) {
+        canId = 200;
+      } else if (canId > 200) {
+        canId = 100;
       }
+    }
 
-      /* Start the Transmission process */
-      if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, TxData) != HAL_OK)
-      {
-        //HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-        /* Transmission request Error */
-        Error_Handler();
+    /* Start the Transmission process */
+    if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, TxData) != HAL_OK)
+    {
+      /* Transmission request Error */
+      HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+      HAL_GPIO_WritePin(WARN_BATT_GPIO_Port, WARN_BATT_Pin, GPIO_PIN_SET);
+      Error_Handler();
+    }
+
+    /* Check if a message is come */
+    FDCAN_RxHeaderTypeDef RxHeader;
+    uint8_t               RxData[8];
+    HAL_StatusTypeDef canRequest = HAL_FDCAN_GetRxMessage(&hfdcan1, FDCAN_RX_FIFO0, &RxHeader, RxData);
+    if (canRequest == HAL_OK) {
+      TxHeader.Identifier = RxHeader.Identifier;
+      if (RxHeader.Identifier == 110) {
         HAL_GPIO_WritePin(WARN_BATT_GPIO_Port, WARN_BATT_Pin, GPIO_PIN_SET);
+        TxData[0] = 2;
+      } else if (RxHeader.Identifier == 111) {
+        HAL_GPIO_WritePin(WARN_BATT_GPIO_Port, WARN_BATT_Pin, GPIO_PIN_RESET);
+        TxData[0] = 1;
       }
 
-    } else {
-      //HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_1);
-      HAL_GPIO_TogglePin(PIL_ALIM_3_GPIO_Port, PIL_ALIM_3_Pin);
+      HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, TxData);
     }
-    osDelay(1000);
+    osDelay(500);
   }
   /* USER CODE END StartDefaultTask */
 }
